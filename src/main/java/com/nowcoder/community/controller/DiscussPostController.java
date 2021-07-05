d3294672d105611ff9b49820b69b4e5d29acf740
csushl <csushl@163.com>
2021-07-05 16:43:40 +0800

    显示评论

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -1,7 +1,10 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Comment;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.CommentService;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
@@ -14,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.*;
+
+import static com.nowcoder.community.util.CommunityConstant.ENTITY_TYPE_COMMENT;
+import static com.nowcoder.community.util.CommunityConstant.ENTITY_TYPE_POST;
 
 @Controller
 @RequestMapping("/discuss")
@@ -29,6 +35,9 @@ public class DiscussPostController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -49,14 +58,79 @@ public class DiscussPostController {
         return CommunityUtil.getJSONString(0, "发布成功!");
     }
 
+//    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+//    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+//        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+//        model.addAttribute("post", discussPost);
+//        // 但是有个问题，我们这个表里得到的是 ID 但是我们在页面上得到的肯定是想得到用户的头像啊或者名字，我们可以通过得到的 ID 查帖子作者
+//        User user = userService.findUserById(discussPost.getUserId());
+//        model.addAttribute("user", user);
+//        return "/site/discuss-detail";
+//    }
+
     @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
-        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
-        model.addAttribute("post", discussPost);
-        // 但是有个问题，我们这个表里得到的是 ID 但是我们在页面上得到的肯定是想得到用户的头像啊或者名字，我们可以通过得到的 ID 查帖子作者
-        User user = userService.findUserById(discussPost.getUserId());
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page) {
+        // 帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        // 作者
+        User user = userService.findUserById(post.getUserId());
         model.addAttribute("user", user);
+
+        // 评论分页信息
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        page.setRows(post.getCommentCount());
+
+        // 评论: 给帖子的评论
+        // 回复: 给评论的评论
+        // 评论列表
+        List<Comment> commentList = commentService.findCommentsByEntity(
+                ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimit());
+        // 评论VO列表
+        List<Map<String, Object>> commentVoList = new ArrayList<>();
+        if (commentList != null) {
+            for (Comment comment : commentList) {
+                // 评论VO
+                Map<String, Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment", comment);
+                // 作者
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+
+                // 回复列表
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE);
+                // 回复VO列表
+                List<Map<String, Object>> replyVoList = new ArrayList<>();
+                if (replyList != null) {
+                    for (Comment reply : replyList) {
+                        Map<String, Object> replyVo = new HashMap<>();
+                        // 回复
+                        replyVo.put("reply", reply);
+                        // 作者
+                        replyVo.put("user", userService.findUserById(reply.getUserId()));
+                        // 回复目标
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                        replyVo.put("target", target);
+
+                        replyVoList.add(replyVo);
+                    }
+                }
+                commentVo.put("replys", replyVoList);
+
+                // 回复数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+
+                commentVoList.add(commentVo);
+            }
+        }
+
+        model.addAttribute("comments", commentVoList);
+
         return "/site/discuss-detail";
     }
+
 
 }
--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -25,4 +25,21 @@ public interface CommunityConstant {
      * 记住状态的登录凭证超时时间
      */
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
+
+    // 因为熟悉了 mysql 的表，我们知道我的评论是有着对应的实体类型评论，它是一个常量代表，所以要在我们确定好的常量接口里面声明这些常量，然后再去 CommentController 里面重构 getDiscussPost 方法
+    /**
+     * 实体类型：帖子
+     */
+    int ENTITY_TYPE_POST = 1;
+
+    /**
+     * 实体类型：评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
+
+    /**
+     * 实体类型: 用户
+     */
+    int ENTITY_TYPE_USER = 3;
+
 }
