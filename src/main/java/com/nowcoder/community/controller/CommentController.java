d21fd3f1d79438695bd9036bae0f04b77c86edb8
csushl <csushl@163.com>
2021-07-05 16:56:41 +0800

    添加评论

---
+++ src/main/java/com/nowcoder/community/controller/CommentController.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.service.CommentService;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController implements CommunityConstant {
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    // 页面会提交内容和两个内容（实体类型和实体 ID）
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
+        // 得到当前用户的 ID
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        commentService.addComment(comment);
+        return "redirect:/discuss/detail/" + discussPostId;
+    }
+}
